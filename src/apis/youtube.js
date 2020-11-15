211782f5ecb2bee70577fb250b9cf12ed8efad1d
Zachery Converse <zacheryconverse@gmail.com>
2020-11-15 16:35:10 -0700

    config youtube api

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY = 'AIzaSyDpoeGvvHVQKY_G1Gzg2F8AJ3Enfx9ncgk';
+
+export default axios.create({
+  baseURL: 'https://www.googleapis.com/youtube/v3',
+  params: {
+    part: 'snippet',
+    maxResults: 5,
+    key: KEY
+  }
+});
